e711b664c0682748fec0ec8e01b640244d87bb5a
Mathieu Schroeter <mathieu@schroetersa.ch>
2024-10-02 13:59:06 +0200

    Add an initial and incomplete blitz builder

---
+++ blitzify.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const path = require('path');
+
+/**
+ * Retrieve the list of available commands.
+ *
+ * @returns {Object} The list and definitions of commands.
+ */
+exports.xcraftCommands = function () {
+  return require('./lib/service.js')(
+    path.basename(__filename, '.js'),
+    'blitz-builder'
+  );
+};
